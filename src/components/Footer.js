f73730ef7e9aefce195382399a58e6034793a28e
Lohmarr <119974080+Lohmarr@users.noreply.github.com>
2023-05-19 23:33:16 -0400

    Project component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import githubImg from '../images/github-mark.svg'
+import linkedInImg from '../images/linkedin.svg'
 
  const Footer = () => {
   return (
     <footer>
       <div className="footer-content">
-        <a href="github-link" target="https://github.com/Lohmarr" rel="noopener noreferrer"><i className="fa fa-github" aria-hidden="true"></i></a>
-        <a href="linkedin-link" target="/" rel="noopener noreferrer"><i className="fa fa-linkedin" aria-hidden="true"></i></a>
-        <a href="twitter-link" target="/" rel="noopener noreferrer"><i className="fa fa-twitter" aria-hidden="true"></i></a>
+        <a href="github-link" target="https://github.com/Lohmarr" rel="noopener noreferrer">
+        <img src={githubImg} alt='Github logo' />
+        </a>
+        <a href="linkedin-link" target="/" rel="noopener noreferrer">
+        <img src={linkedInImg} alt='Linked In logo' />
+        </a>
       </div>
     </footer>
   );
