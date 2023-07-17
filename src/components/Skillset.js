f5ae79fed648ec77f26542ddc530110344009b6c
Lohmarr <119974080+Lohmarr@users.noreply.github.com>
2023-07-17 17:26:48 -0400

    refining pages

--- src/components/Skillset.js
+++ src/components/Skillset.js
@@ -1,32 +1,51 @@
-import React from "react";
+import React from 'react';
 
-function SkillSet() {
+const SkillList = () => {
+  const skillsets = [
+    {
+      category: 'Programming Languages',
+      skills: ['JavaScript', 'CSS', 'HTML5'],
+    },
+    {
+      category: 'Databases',
+      skills: ['GraphQL', 'MongoDB', 'MySQL'],
+    },
+    {
+      category: 'Cloud Services',
+      skills: ['Heroku'],
+    },
+    {
+      category: 'Frameworks and Libraries',
+      skills: ['React.js', 'Node.js', 'Express.js', 'Handlebars.js','jQuery', 'Bootstrap', 'Tailwind'],
+    },
+    {
+      category: 'Version Control',
+      skills: ['Git', 'GitHub', 'GitLab'],
+    },
+    {
+      category: 'Other Concepts and Technologies',
+      skills: ['RESTful API', 'Agile', 'Object-Oriented Programming (OOP)', 'Progressive Web Applications (PWAs)', 'User Authentication', 'Unit Testing', 'Full-stack development', 'MERN Stack (MongoDB, Express.js, React, Node.js)'],
+    },
+    {
+      category: 'Upcoming Skills From Continuation Courses at UNCC School of Professional Studies',
+      skills: ['Python', 'AWS', 'C#', 'Java'],
+    },
+  ];
+
   return (
     <div>
-      <h3>Acquired Skillsets</h3>
-      <ul className="list-group mb-2">
-        <li className="list-group-item">HTML</li>
-        <li className="list-group-item">CSS</li>
-        <li className="list-group-item">Git</li>
-        <li className="list-group-item">Bash</li>
-        <li className="list-group-item">JavaScript/jQuery</li>
-        <li className="list-group-item">
-          MERN Stack (MongoDB, Express.js, React.js, Node.js)
-        </li>
-        <li className="list-group-item">MySQL</li>
-        <li className="list-group-item">GraphQL</li>
-        <li className="list-group-item">API Design</li>
-      </ul>
-      <h3>Upcoming Skillsets</h3>
-      <div className="container">
-        <ul className="list-group">
-          <li className="list-group-item">Python</li>
-          <li className="list-group-item">Java</li>
-          <li className="list-group-item">Amazon Web Services</li>
-          <li className="list-group-item">C#</li>
-        </ul>
-      </div>
+      {skillsets.map((skillset, index) => (
+        <div key={index}>
+          <h4>{skillset.category}</h4>
+          <ul>
+            {skillset.skills.map((skill, skillIndex) => (
+              <li key={skillIndex}>{skill}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
-}
-export default SkillSet;
+};
+
+export default SkillList;
