089ad2e1d71e944f41c3b1055d0bfd7ff019e525
Lohmarr <119974080+Lohmarr@users.noreply.github.com>
2023-07-17 17:52:40 -0400

    Revert "refining pages"
    This reverts commit f5ae79fed648ec77f26542ddc530110344009b6c.

---
+++ src/pages/Resume.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Skillset from '../components/Skillset'
+
+ const Resume = () => {
+  return (
+    <section>
+      <div className="resume-content">
+        <h2>Resume</h2>
+        <p>Download my resume <a href="path-to-resume" target="_blank" rel="noopener noreferrer">here</a>.</p>
+        <h3>Proficiencies</h3>
+        <ul>
+          <Skillset/>
+        </ul>
+      </div>
+    </section>
+  );
+};
+ export default Resume;
