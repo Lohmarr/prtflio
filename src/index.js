2f9929595945e27460f716f9c9221425bb0984f2
Lohmarr <119974080+Lohmarr@users.noreply.github.com>
2023-07-17 17:28:56 -0400

    Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom';
-import App from './App';
-import "simpledotcss/simple.min.css";
-import "../src/componentStyles/App.css"
-
-ReactDOM.render(<App />, document.getElementById('root'));
